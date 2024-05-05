a41914d2c5755cd5a01da0cfa040660227bd2055
Vlad Shevlyakov <vlad.shew12@gmail.com>
2024-05-05 19:39:01 +0300

    Полная функциональность загрузки файлов при создании записи

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,12 +1,71 @@
-import { Button, FileInput, Label, Select, TextInput } from "flowbite-react";
+import {
+    Alert,
+    Button,
+    FileInput,
+    Label,
+    Select,
+    TextInput,
+} from "flowbite-react";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 import ReactQuill from "react-quill";
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from "firebase/storage";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import "react-quill/dist/quill.snow.css";
 import "react-datepicker/dist/react-datepicker.css";
 import ru from "date-fns/locale/ru";
+import { app } from "../firebase";
 
 export default function CreatePost() {
+    const [file, setFile] = useState(null);
+    const [fileUploadProgress, setFileUploadProgress] = useState(null);
+    const [fileUploadError, setFileUploadError] = useState(null);
+    const [formData, setFormData] = useState({});
+    const handleUploadFile = async () => {
+        try {
+            if (!file) {
+                setFileUploadError("Пожалуйста, выберите файл");
+                return;
+            }
+            setFileUploadError(null);
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + "-" + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    setFileUploadProgress(progress.toFixed(0));
+                },
+                (error) => {
+                    setFileUploadError("Не удалось загрузить файл");
+                    setFileUploadProgress(null);
+                    console.log(error);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(
+                        (downloadURL) => {
+                            setFileUploadProgress(null);
+                            setFileUploadError(null);
+                            setFormData({ ...formData, image: downloadURL });
+                        }
+                    );
+                }
+            );
+        } catch (error) {
+            setFileUploadError("Не удалось загрузить файл");
+            setFileUploadProgress(null);
+            console.log(error);
+        }
+    };
     const [forms, setForms] = useState([{}]);
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
@@ -54,16 +113,34 @@ export default function CreatePost() {
                             className="flex gap-4 items-center justify-between border-4 border-teal-500
                         border-dotted p-3"
                         >
-                            <FileInput type="file" accept="image/*" />
+                            <FileInput
+                                type="file"
+                                accept="*"
+                                onChange={(e) => setFile(e.target.files[0])}
+                            />
                             <Button
                                 type="button"
                                 gradientDuoTone="purpleToBlue"
                                 size="sm"
                                 outline
+                                onClick={handleUploadFile}
+                                disabled={fileUploadProgress}
                             >
-                                Загрузить файл
+                                {fileUploadProgress ? (
+                                    <div className="w-16 h-16">
+                                        <CircularProgressbar
+                                            value={fileUploadProgress}
+                                            text={`${fileUploadProgress || 0}%`}
+                                        />
+                                    </div>
+                                ) : (
+                                    "Загрузить файл"
+                                )}
                             </Button>
                         </div>
+                        {fileUploadError && (
+                            <Alert color="failure">{fileUploadError}</Alert>
+                        )}
                         {index === forms.length - 1 && (
                             <div className="flex gap-4 flex-col sm:flex-row">
                                 <div className="flex flex-col">
